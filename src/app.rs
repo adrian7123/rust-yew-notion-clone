8c9678a0b803fc800f8e6b2d3b9d8643d5855b60
Adrian_Bueno <adrian.s.bueno1@gmail.com>
2023-09-22 13:06:22 -0300

    up

--- src/app.rs
+++ src/app.rs
@@ -4,9 +4,7 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <p class="bg-red-100">{"Test!"}</p>
         </main>
     }
 }
